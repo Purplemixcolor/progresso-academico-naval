4edc0fd69ffb24145a0db28fd1ba50d7d5c6e120
Purple <purplemix@proton.me>
2026-06-24 13:36:30 -0400

    Atualiza identidade visual do app

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "progresso-academico-v17";
+const CACHE_NAME = "progresso-academico-v18";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=17",
+  "./app.js?v=18",
   "./manifest.webmanifest",
   "./app-icon-192.png",
   "./app-icon-512.png"
