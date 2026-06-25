88eb5cc2c7c775c30e9cfeae5df0dc191c02a146
Purple <purplemix@proton.me>
2026-06-25 19:20:08 -0400

    Corrige quebra dos cards de indicadores

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "progresso-academico-v20";
+const CACHE_NAME = "progresso-academico-v21";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
-  "./app.js?v=20",
+  "./app.js?v=21",
   "./manifest.webmanifest",
   "./app-icon-192.png",
   "./app-icon-512.png"
